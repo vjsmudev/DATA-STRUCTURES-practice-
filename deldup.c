d376d4e40cee947406aa4da9fdb78ca16351388d
dev_0419 <52438900+vjsmudev@users.noreply.github.com>
2022-12-02 01:45:03 +0530

    Update deldup.c

--- deldup.c
+++ deldup.c
@@ -1,3 +1,8 @@
+/*You're given the pointer to the head node of a sorted singly linked list, where the
+data in the nodes is in ascending order. Delete as few nodes as possible so that the
+list does not contain any value more than once (deleting duplicates). The given head
+pointer may be null indicating that the list is empty.*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
